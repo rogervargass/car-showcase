f6c7cee176c9cddf63bcfcb504d7a8eed4de42cc
Roger Vargas <roger.vargas@cwi.com.br>
2023-09-06 17:08:42 -0300

    fix: eslint error

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -58,7 +58,7 @@ const SearchManufacturer = ({
                   value={query}
                   className='search-manufacturer__option'
                 >
-                  Create "{query}"
+                  {`Create "${query}"`}
                 </Combobox.Option>
               ) : (
                 filteredManufacturers.map(item => (
