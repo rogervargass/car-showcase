c1c6edb9594b556712c02c936cd2481dd9124918
Roger Vargas <roger.vargas@cwi.com.br>
2023-09-06 09:17:24 -0300

    feat: car details modal

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import { calculateCarRent, generateCarImageUrl } from '@utils'
 import { CarProps } from '@types'
 import CustomButton from './CustomButton'
+import CarDetails from './CarDetails'
 
 interface CarCardProps {
   car: CarProps
@@ -38,7 +39,7 @@ const CarCard = ({ car }: CarCardProps) => {
 
       <div className='relative w-full h-40 my-3 object-contain'>
         <Image
-          src='/hero.png'
+          src={generateCarImageUrl(car)}
           alt='car model'
           fill
           priority
@@ -79,6 +80,12 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
+
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   )
 }
